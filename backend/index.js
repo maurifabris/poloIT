19f59a9189b44979b2d505ad652afab9af0a8f52
maurifabris <maurifabris91@gmail.com>
2023-10-29 12:15:31 -0300

    CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express, { request } from "express"
+import express, { request, response } from "express"
 import { PORT, mongoDBURL} from "./config.js";
 import mongoose from 'mongoose'
 import { ent } from "./models/enterprisesModel.js";
@@ -50,6 +50,64 @@ app.get('/enterprises', async (request, response)=>{
     } catch (err){
         console.log(err.message)
         response.status(500).send({ message: err.message})
+    }
+})
+
+
+app.get('/enterprises/:id', async (request, response)=>{
+    try {
+        const { id } = request.params;
+
+        const enterprise = await ent.findById(id)
+
+        return response.status(200).json({
+          enterprise
+        })
+    } catch (err){
+        console.log(err.message)
+        response.status(500).send({ message: err.message})
+    }
+})
+
+// update enterprises
+
+app.put('/enterprises/:id', async(request, response)=>{
+    try{
+        if(
+            !request.body.name ||
+            !request.body.description
+        ) {
+            return response.status(400).send({
+                message:'Plase, send all params.'
+            })
+        }
+        const { id } = request.params
+
+        const result = await ent.findByIdAndUpdate(id, request.body)
+        
+        if(!result){
+            return response.status(404).json({ message:'Enterprise not found'})
+        }
+        return response.status(200).send({message: 'Enterprise updated.'})
+    } catch (err){
+        console.log(err.message)
+        response.status(500).send({message: err.message})
+    }
+})
+
+// Delete Rout
+
+app.delete('/enterprises/:id', async(request, response)=>{
+    try {
+        const { id } = request.params
+        const result = await ent.findByIdAndDelete(id) 
+        if(!result){
+            return response.status(400).useChunkedEncodingByDefault({message:'Enterprise not found'})
+        }
+        return response.status(200).send({message: 'Enterprise deleted'})
+    } catch (err) {
+        console.log(err.message)
+        response.status(500).send({message: err.message})
     }
 })
 
