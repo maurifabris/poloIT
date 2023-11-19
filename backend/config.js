4d14fdec4d1c633aae423f59fce05335880c9bfc
maurifabris <maurifabris91@gmail.com>
2023-11-19 12:21:20 -0300

    commit final

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,13 @@
+import dotenv from "dotenv"
+
+dotenv.config({path: "../.env"})
+
+const mongopass = process.env.mongopass
+
+
 export const PORT = 5555
 
 
 // URL OF DATABASE it 
-export const mongoDBURL = 'mongodb+srv://prueba:123@cluster0.8obp27j.mongodb.net/?retryWrites=true&w=majority'
+export const mongoDBURL = `mongodb+srv://prueba:${mongopass}@cluster0.8obp27j.mongodb.net/?retryWrites=true&w=majority`
 
